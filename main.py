cc19d0c6424ed22b7940270b98c09996e9d45eae
JakubSzwajka <szwajkajakub@gmail.com>
2020-12-27 23:04:57 +0100

    Bird class added

---
+++ main.py
@@ -0,0 +1,38 @@
+import pygame 
+import neat 
+import time 
+import os 
+import random 
+
+
+import pygame
+
+import Settings as set 
+from Bird import Bird
+
+def draw_window(win, bird: "Bird"):
+    win.blit(set.BG_IMG, (0,0))
+    bird.draw(win)
+    pygame.display.update()
+
+def main():
+
+    bird = Bird(200, 200 )
+    win = pygame.display.set_mode((set.WIN_WIDTH, set.WIN_HEIGHT))
+
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(30)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                run = False
+        
+        bird.move()
+        draw_window(win, bird)
+    pygame.quit()
+    quit()
+
+main()
